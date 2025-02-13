8d8482afc068a9b6bce2edaf38e612d38aba8df8
dean <nicklasfrederiksen@gmail.com>
2025-02-13 16:17:07 +0100

    first

--- 6-higher-order-functions-DOM/cat-facts/cat-facts.js
+++ 6-higher-order-functions-DOM/cat-facts/cat-facts.js
@@ -6,6 +6,7 @@
 
 document.querySelector("button").addEventListener("click", getId);
 
+//This can be used in exercise D. This function fetches the ID of an image.
 function getId(event) {
     console.log(event.target.classList);
 }
