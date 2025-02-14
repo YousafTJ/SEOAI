b14b77c2c40fb5d8b88be3144ce97536e5e1c237
dean <nicklasfrederiksen@gmail.com>
2025-02-14 09:39:09 +0100

    first

--- 6-higher-order-functions-DOM/examples/higher-order-functions.js
+++ 6-higher-order-functions-DOM/examples/higher-order-functions.js
@@ -1,10 +1,24 @@
 //Array of numbers
-const numbers = [1, 312, 2, 1243, 45, 99, 231, 4, 5, 5, 5];
+const numbers = [false, "asdljh",true, 1, 312, 2, 1243, 45, 99, 231, 4, 5, 5, 5];
 
 //Callback function
+function callBack(fisk){
+    // console.log(fisk);
+}
 
 //Foreach loop
+numbers.forEach(callBack);
 
 //Callback function
+function callbackHigher(index){
+    if(index > 0){
+        return true;
+    }
+    else{
+        return false
+    }
+}
 
 //Filter
+const newArray = numbers.filter(callbackHigher);
+console.log(newArray);
