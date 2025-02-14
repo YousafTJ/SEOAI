1b3553c309c3c6246a310dba5516c36fd02d79a2
dean <nicklasfrederiksen@gmail.com>
2025-02-14 10:56:35 +0100

    first

--- 6-higher-order-functions-DOM/examples/higher-order-functions.js
+++ 6-higher-order-functions-DOM/examples/higher-order-functions.js
@@ -21,4 +21,20 @@ function callbackHigher(index){
 
 //Filter
 const newArray = numbers.filter(callbackHigher);
-console.log(newArray);
+// console.log(newArray);
+
+//Exercise D
+const products = [
+    { name: "Keyboard", category: "Electronics", price: 29.99 },
+    { name: "T-Shirt",  category: "Clothing",    price: 9.99  },
+    { name: "Mug",      category: "Kitchen",     price: 5.0   },
+    { name: "TV",       category: "Electronics", price: 299.99},
+    { name: "Jeans",    category: "Clothing",    price: 39.99 },
+    { name: "Blender",  category: "Kitchen",     price: 45.0  },
+];
+
+products.forEach(productCallback)
+
+function productCallback(elem){
+    console.log(elem);
+}
